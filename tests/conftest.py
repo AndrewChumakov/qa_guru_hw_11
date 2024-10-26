56229f3dba973ed358e302f23e2d8e072a297d6e
Andrew Chumakov <andrew.a.chumakov@yandex.ru>
2024-10-26 17:47:16 +0300

    add attach

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,36 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_driver():
-    driver_options = webdriver.FirefoxOptions()
-    driver_options.page_load_strategy = "eager"
-    browser.config.driver_options = driver_options
     browser.config.base_url = "https://demoqa.com"
+    options = Options()
+
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVideo": False
+        }
+    }
+
+    options.page_load_strategy = "eager"
+    options.capabilities.update(capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
     yield
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
     browser.quit()
